f82abc7319913a6008127c44f26d937dbcf0ca99
LeeSeungHun <dos07008@naver.com>
2022-08-20 01:52:17 +0900

    Relay 없이 사용 데이터 fetch하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { useState, useEffect } from "react";
+import fetchGraphQL from "./fetchGraphQL";
+
 function App() {
-  return <div>ㅇ</div>;
+  const [name, setName] = useState(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetchGraphQL(`
+      query RepositoryNameQuery {
+        # feel free to change owner/name here
+        repository(owner: "facebook" name: "relay") {
+          name
+        }
+      }
+    `)
+      .then((response) => {
+        if (!isMounted) {
+          return;
+        }
+        const data = response.data;
+        setName(data.repository.name);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{name != null ? `Repository: ${name}` : "Loading"}</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
